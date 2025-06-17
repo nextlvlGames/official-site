13525d764d217e04402b7c93130d96cc5d330145
suryabhai <suryaprakashdas921@gmail.com>
2025-06-17 15:47:43 +0530

    intial commit

--- Official-Site/vite.config.js
+++ Official-Site/vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser global polyfills
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Ensure no unnecessary chunks are created
+      },
+    },
+  },
 })
 
